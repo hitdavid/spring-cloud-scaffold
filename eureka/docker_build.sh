2b3bbcbd92580fce3df8c5e4722e89307a9803a2
杜宇 <duyub@chanjet.com>
2017-06-14 22:15:24 +0800

    fix

--- eureka/docker_build.sh
+++ eureka/docker_build.sh
@@ -7,4 +7,4 @@ docker build -t hitdavid/eureka .
 docker ps | grep 0.0.0.0:8761 | awk '{print $1}' | xargs docker stop
 read -p "请输入HOST:" host
 read -p "请输入PORT:" port
-docker run -d -p 8761:8761 -e HOST="$host" -e PORT="$port" hitdavid/zuul
+docker run -d -p 8761:8761 -e HOST="$host" -e PORT="$port" hitdavid/eureka
