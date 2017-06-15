344c0e6efe3cc70df0211b99fd90c239985b2032
杜宇 <duyub@chanjet.com>
2017-06-15 10:20:20 +0800

    update

--- rest-api/docker/onStart.sh
+++ rest-api/docker/onStart.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 java -server -jar /usr/local/app/jar_bin.jar --server.port=8001
+
--- zuul/docker/onStart.sh
+++ zuul/docker/onStart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-LC_ALL=C ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | grep -v ".1 " | cut -d: -f2|awk '{print $1}' | xagrs java -server -jar /usr/local/app/jar_bin.jar --server.port=8000 --eureka.instance.ip-address:
+java -server -jar /usr/local/app/jar_bin.jar --server.port=8000 --eureka.instance.ip-address:'${MACHINE_IP}'
