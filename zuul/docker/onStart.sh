4596078ca7a1901c64e06b6c60214c93115d6a37
杜宇 <duyub@chanjet.com>
2017-06-15 10:10:28 +0800

    update

--- zuul/docker/onStart.sh
+++ zuul/docker/onStart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -server -jar /usr/local/app/jar_bin.jar --server.port=8000
+LC_ALL=C ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | grep -v ".1 " | cut -d: -f2|awk '{print $1}' | xagrs java -server -jar /usr/local/app/jar_bin.jar --server.port=8000 --eureka.instance.ip-address:
