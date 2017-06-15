894cb5fda95ec7a5481ce1dd1fd2f009cc2a5eae
杜宇 <duyub@chanjet.com>
2017-06-15 10:31:51 +0800

    update

--- zuul/docker_build.sh
+++ zuul/docker_build.sh
@@ -6,5 +6,4 @@ cp ./target/jar_bin.jar ./docker/jar_bin.jar
 cd docker
 docker build -t hitdavid/zuul .
 docker ps | grep 0.0.0.0:8000 | awk '{print $1}' | xargs docker stop
-export HOST_IP = LC_ALL=C ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | grep -v ".1 " | cut -d: -f2|awk '{print $1}'
-docker run -d -p 8000:8000 -e MACHINE_IP="$HOST_IP" hitdavid/zuul
+LC_ALL=C ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | grep -v ".1 " | cut -d: -f2|awk '{print $1}'| xargs -t -I{} docker run -d -p 8000:8000 -e MACHINE_IP={} hitdavid/zuul
