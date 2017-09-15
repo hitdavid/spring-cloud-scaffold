ddd3f70a4f664bebf5e2fed87593cd9a2b957278
杜宇 <duyub@chanjet.com>
2017-09-15 08:17:56 +0800

    update

--- eureka/docker_build.sh
+++ eureka/docker_build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-./mvnw clean package
+mvn clean package
 rm -f ./docker/jar_bin.jar
 cp ./target/jar_bin.jar ./docker/jar_bin.jar
 cd docker
 docker build -t hitdavid/eureka .
-docker tag hitdavid/eureka registry.gzq.chanjet.com/hitdavid/eureka:2017.06.15.012
-docker push registry.gzq.chanjet.com/hitdavid/eureka:2017.06.15.012
+docker tag hitdavid/eureka hitdavid/eureka:2017.09.15.001
+docker push hitdavid/eureka:2017.09.15.001
